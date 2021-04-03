84cc4be882e33956491f28c3340a9570592cacb8
Igor Simões <ibps@cin.ufpe.br>
2021-04-03 17:59:44 -0300

    Create Activity model

---
+++ server/src/Models/ActivityModel.ts
@@ -0,0 +1,12 @@
+import { BaseCategoryModel, CategoryModel } from './CategoryModel';
+
+export interface BaseActivityModel {
+  id: number;
+}
+
+export interface ActivityModel extends BaseActivityModel {
+  category: CategoryModel;
+  title: string;
+  description?: string;
+  duration: string;
+}
