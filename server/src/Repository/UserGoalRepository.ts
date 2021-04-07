48e64f54a1343b1b1c9477db3a03907a55164e06
Vitor Cardim <vcm3@cin.ufpe.br>
2021-04-07 13:54:16 -0300

    Queries a serem usadas e ajustes.

---
+++ server/src/Repository/UserGoalRepository.ts
@@ -0,0 +1,6 @@
+import { Request, Response, NextFunction } from 'express';
+import { Connect, Query } from '../config/mysql';
+
+//Falta Select, insert e delete, mas depende do como vai ser o dito pelo front
+
+export default { };
