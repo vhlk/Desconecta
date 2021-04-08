beb2427a8e902205b3c899467c48fc4db2321d96
JuliaFC <mjfc@cin.ufpe.br>
2021-04-08 11:46:39 -0400

    Implementa rota e repository para a entidade Favorite

--- server/src/Repository/FavoritesRepository.ts
+++ server/src/Repository/FavoritesRepository.ts
@@ -1,7 +1,112 @@
 import { Request, Response, NextFunction } from 'express';
 import { Connect, Query } from '../config/mysql';
 
-//Insert Into Favorites(User_ID, Activities_ID) Values (UserIdInput, ActivitiesIdInput)
-//Delete from Favorites WHERE Favorites.User_ID = UserIdInput AND Favorites.Activities_ID = ActivitiesIdInput
-//Select Favorites.Activities_ID, Activities.Title, Activities.Description, Activities.Duration, Category.Name From Favorites Join Activities On Activities.ID = Favorites.Activities_ID AND Favorites.Activities_ID = ActivitiesIdInput Join Category On Category.ID = Activities.Category_ID WHERE Favorites.User_ID = UserIdInput 
-export default { };
+const getAllFavorites = (req: Request, res: Response, next: NextFunction) => {
+    const query = 'SELECT * FROM Favorites';
+
+    Connect()
+    .then(connection => {
+        Query(connection, query)
+        .then(results => {
+            return res.status(200).json(results);
+        })
+        .catch(err => {
+            return res.status(500).json({
+                message: err.message,
+                err
+            });
+        })
+        .finally(() => {
+            connection.end();
+        })
+    })
+    .catch(err => {
+        return res.status(500).json({
+            message: err.message,
+                err
+        });
+    });
+};
+
+const getFavorite = (req: Request, res: Response, next: NextFunction) => {
+    const query = `Select Favorites.Activities_ID, Activities.Title, Activities.Description, Activities.Duration, Category.Name From Favorites Join Activities On Activities.ID = Favorites.Activities_ID AND Favorites.Activities_ID = ActivitiesIdInput Join Category On Category.ID = ${req.params.favoriteId} WHERE Favorites.User_ID = ${req.params.userId}`;
+
+    Connect()
+    .then(connection => {
+        Query(connection, query)
+        .then(results => {
+            return res.status(200).json(results);
+        })
+        .catch(err => {
+            return res.status(500).json({
+                message: err.message,
+                err
+            });
+        })
+        .finally(() => {
+            connection.end();
+        })
+    })
+    .catch(err => {
+        return res.status(500).json({
+            message: err.message,
+                err
+        });
+    });
+};
+
+const insertFavorite = (req: Request, res: Response, next: NextFunction) => {
+    const query = `Insert into FavoritesFavorites(User_ID, Activities_ID) Values (${req.params.userId}, ${req.params.favoriteId})`;
+
+    Connect()
+    .then(connection => {
+        Query(connection, query)
+        .then(results => {
+            return res.status(200).json(results);
+        })
+        .catch(err => {
+            return res.status(500).json({
+                message: err.message,
+                err
+            });
+        })
+        .finally(() => {
+            connection.end();
+        })
+    })
+    .catch(err => {
+        return res.status(500).json({
+            message: err.message,
+                err
+        });
+    });
+};
+
+const deleteFavorite = (req: Request, res: Response, next: NextFunction) => {
+    const query = `Delete from Favorites WHERE Favorites.User_ID = ${req.params.userId} AND Favorites.Activities_ID = ${req.params.favoriteId}`;
+
+    Connect()
+    .then(connection => {
+        Query(connection, query)
+        .then(results => {
+            return res.status(200).json(results);
+        })
+        .catch(err => {
+            return res.status(500).json({
+                message: err.message,
+                err
+            });
+        })
+        .finally(() => {
+            connection.end();
+        })
+    })
+    .catch(err => {
+        return res.status(500).json({
+            message: err.message,
+                err
+        });
+    });
+};
+
+export default { getAllFavorites, getFavorite, insertFavorite, deleteFavorite };
