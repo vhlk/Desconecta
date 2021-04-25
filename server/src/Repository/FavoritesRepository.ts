88ccd9e2001e27372bd8aa4104bb1e2e7479152e
Igor Simões <ibps@cin.ufpe.br>
2021-04-25 15:03:26 -0300

    Add ImageLink as return from getUserFavorite

--- server/src/Repository/FavoritesRepository.ts
+++ server/src/Repository/FavoritesRepository.ts
@@ -2,83 +2,83 @@ import { Request, Response, NextFunction } from 'express';
 import { Connect, Query } from '../config/mysql';
 
 const getUserFavorite = (req: Request, res: Response, next: NextFunction) => {
-    const query = `Select Favorites.Activities_ID, Activities.Title, Activities.Description, Activities.Duration, Category.Name From Favorites Join Activities On Activities.ID = Favorites.Activities_ID Join Category On Category.ID = Activities.Category_ID WHERE Favorites.User_ID = ${req.params.userId}`;
+  const query = `Select Favorites.Activities_ID, Activities.Title, Activities.Description, Activities.Duration, Activities.ImageLink, Category.Name From Favorites Join Activities On Activities.ID = Favorites.Activities_ID Join Category On Category.ID = Activities.Category_ID WHERE Favorites.User_ID = ${req.params.userId}`;
 
-    Connect()
+  Connect()
     .then(connection => {
-        Query(connection, query)
+      Query(connection, query)
         .then(results => {
-            return res.status(200).json(results);
+          return res.status(200).json(results);
         })
         .catch(err => {
-            return res.status(500).json({
-                message: err.message,
-                err
-            });
+          return res.status(500).json({
+            message: err.message,
+            err,
+          });
         })
         .finally(() => {
-            connection.end();
-        })
+          connection.end();
+        });
     })
     .catch(err => {
-        return res.status(500).json({
-            message: err.message,
-                err
-        });
+      return res.status(500).json({
+        message: err.message,
+        err,
+      });
     });
 };
 
 const insertFavorite = (req: Request, res: Response, next: NextFunction) => {
-    const query = `Insert into Favorites(User_ID, Activities_ID) Values (${req.params.userId}, ${req.params.activityId})`;
+  const query = `Insert into Favorites(User_ID, Activities_ID) Values (${req.params.userId}, ${req.params.activityId})`;
 
-    Connect()
+  Connect()
     .then(connection => {
-        Query(connection, query)
+      Query(connection, query)
         .then(results => {
-            return res.status(200).json(results);
+          return res.status(200).json(results);
         })
         .catch(err => {
-            return res.status(500).json({
-                message: err.message,
-                err
-            });
+          return res.status(500).json({
+            message: err.message,
+            err,
+          });
         })
         .finally(() => {
-            connection.end();
-        })
+          connection.end();
+        });
     })
     .catch(err => {
-        return res.status(500).json({
-            message: err.message,
-                err
-        });
+      return res.status(500).json({
+        message: err.message,
+        err,
+      });
     });
 };
 
 const deleteFavorite = (req: Request, res: Response, next: NextFunction) => {
-    const query = `Delete from Favorites WHERE Favorites.User_ID = ${req.params.userId} AND Favorites.Activities_ID = ${req.params.activityId}`;
+  const query = `Delete from Favorites WHERE Favorites.User_ID = ${req.params.userId} AND Favorites.Activities_ID = ${req.params.activityId}`;
 
-    Connect()
+  Connect()
     .then(connection => {
-        Query(connection, query)
+      Query(connection, query)
         .then(results => {
-            return res.status(200).json(results);
+          return res.status(200).json(results);
         })
         .catch(err => {
-            return res.status(500).json({
-                message: err.message,
-                err
-            });
+          return res.status(500).json({
+            message: err.message,
+            err,
+          });
         })
         .finally(() => {
-            connection.end();
-        })
+          connection.end();
+        });
     })
     .catch(err => {
-        return res.status(500).json({
-            message: err.message,
-                err
-        });
+      return res.status(500).json({
+        message: err.message,
+        err,
+      });
     });
 };
 
