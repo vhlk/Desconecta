790a3f567d028882d01f587af23a463536951654
Victor Hugo <vhlk@cin.ufpe.br>
2021-04-29 01:27:28 -0300

    show loading on favorite page

--- mobile/src/pages/favorites/index.tsx
+++ mobile/src/pages/favorites/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react"
-import { View, Image, StyleSheet, Text, FlatList, ImageBackground, TouchableOpacity } from "react-native"
+import { View, Image, StyleSheet, Text, FlatList, ImageBackground, TouchableOpacity, ActivityIndicator } from "react-native"
 import { RectButton } from "react-native-gesture-handler"
 import { Header, Icon } from "react-native-elements"
 import { useNavigation } from "@react-navigation/native"
@@ -10,7 +10,7 @@ import MainApi from '../../services/ApiModule';
 
 const Item = ({ item }:any) => (
     <View style={styles.card}>
-        <ImageBackground source={{ uri: item.ImageLink }} style={styles.imgbg} imageStyle={{ borderRadius: 10 }}>
+        <ImageBackground source={{ uri: item.ImageLink }} style={styles.imgbg} imageStyle={{ borderRadius: 10 }} progressiveRenderingEnabled={true}>
             <LinearGradient colors={['rgba(9,30,31,0)', 'rgba(9,30,31,0.3)', 'rgba(9,30,31,0.7)', 'rgba(9,30,31,0.9)']} style={styles.gradient}>
                 <View style={{ height: 130, marginHorizontal: 10, justifyContent: "flex-end" }}>
                     <Text style={styles.category}>{item.Name}</Text>
@@ -29,6 +29,7 @@ const Favorites = () => {
     const [userId, setUserId]= useState("-1");
     const [favoriteList, setFavoriteList] = useState<Array<Object>|null>(null);
     const [username, setUsername]= useState("");
+    const [loading, setLoading] = useState(true);
 
     const renderItem = ({ item }:any) => (
         <TouchableOpacity onPress={() => {
@@ -51,6 +52,7 @@ const Favorites = () => {
         async function updateFavorites() {
             MainApi.GetAllFavoritesForUser(userId).then(res => {
                 setFavoriteList(res.data)});
+                setLoading(false);
         }
         async function updateUsername() {
             MainApi.GetUserDataByID(+userId).then(res => setUsername(res.data[0].Name));
@@ -60,6 +62,8 @@ const Favorites = () => {
             updateFavorites();
         }
     },[userId]);
+
+    const keyExtractor = (item:any) => {return item.Activities_ID};
 
     return (
         <>
@@ -85,14 +89,17 @@ const Favorites = () => {
                 }
                 backgroundColor='#f6f7f1'
             />
-            <View style={styles.favoriteList}>
-                {favoriteList !== null && favoriteList.length > 0 &&(
-                <FlatList
-                    data={favoriteList}
-                    keyExtractor={(item:any) => {return item.Activities_ID}}
-                    renderItem={renderItem}
-                />)}
-            </View>
+            {!loading ? (
+                <View style={styles.favoriteList}>
+                    {favoriteList !== null && favoriteList.length > 0 &&(
+                    <FlatList
+                        data={favoriteList}
+                        keyExtractor={keyExtractor}
+                        renderItem={renderItem}
+                        
+                    />)}
+                </View> )
+            : ( <ActivityIndicator size="large" color={'#34a0a4'} style={{flex: 1}} />)}
 
         </>
     );
